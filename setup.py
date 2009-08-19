34544daacbc39e02b4fe8e07d77ddce398152b91
kiorky <kiorky@cryptelium.net>
2009-08-19 11:58:54 +0200

    move

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ setupdir = os.path.abspath(
 )
 os.chdir(setupdir)
 
-name='minitage.recipe'
-version = '1.34'
+name='minitage.recipe.du'
+version = '1.35'
 
 def read(*rnames):
     return open(
@@ -15,22 +15,6 @@ def read(*rnames):
 
 long_description = (
     read('README.txt')
-    + '\n'\
-    + read('src', 'minitage', 'recipe', 'tests', 'shared.txt')
-    + '\n'
-    + read('src', 'minitage', 'recipe', 'tests', 'scripts.txt')
-    + '\n'
-    + read('src', 'minitage', 'recipe', 'tests', 'egg.txt')
-    + '\n'
-    + read('src', 'minitage', 'recipe', 'tests', 'cmmi.txt')
-    + '\n'
-    + read('src', 'minitage', 'recipe', 'tests', 'fetch.txt')
-    + '\n'
-    + read('src', 'minitage', 'recipe', 'tests', 'printer.txt')
-    + '\n'
-    + read('src', 'minitage', 'recipe', 'tests', 'wsgi.txt')
-    + '\n'
-    + read('src', 'minitage', 'recipe', 'tests', 'api.txt')
     + '\n'
     + read('CHANGES.txt')
     + '\n'
@@ -59,13 +43,14 @@ setup(
     license='BSD',
     packages=find_packages('src'),
     package_dir = {'': 'src'},
-    namespace_packages=['minitage', name],
+    namespace_packages=['minitage', 'minitage.recipe', name],
     include_package_data=True,
     zip_safe=False,
     install_requires = [
         'zc.buildout',
         'setuptools',
         'minitage.core',
+        'minitage.recipe.common',
         'iniparse',
     ],
     extras_require={'test': ['IPython', 'zope.testing', 'mocker']},
@@ -76,17 +61,9 @@ setup(
     # workaround when using the 2 recipes in the same buildout.
     entry_points = {
         'zc.buildout' : [
-            'default = %s:Recipe' % name,
-            'du = %s:Recipe' % 'minitage.recipe.du',
-            'fetch = %s:Recipe' % 'minitage.recipe.fetch',
-            'egg = %s:Recipe' % 'minitage.recipe.egg',
-            'printer = %s:Recipe' % 'minitage.recipe.printer',
-            'zdu = %s:Recipe' % 'minitage.recipe.du',
-            'cmmi = %s:Recipe' % 'minitage.recipe.cmmi',
-            'scripts = %s:Recipe' % 'minitage.recipe.scripts',
-            'script = %s:Recipe' % 'minitage.recipe.scripts',
-            'eggs = %s:Recipe' % 'minitage.recipe.scripts',
-            'wsgi = %s:Recipe' % 'minitage.recipe.wsgi',
+            'du = %s:Recipe' % 'minitage.recipe.du.du',
+            'zdu = %s:Recipe' % 'minitage.recipe.du.du',
+            'default = %s:Recipe' % 'minitage.recipe.du.du',
         ]
     },
 )
